47292c97a12312fe28765393a9d146307ebefa65
x7eo <99x7eo@gmail.com>
2022-04-17 08:17:12 -0600

    prefered config

---
+++ config/config.go
@@ -0,0 +1,84 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type config struct {
+	Server   ServerConfig
+	Postgres PostgresConfig
+}
+
+// Postgresql config
+type PostgresConfig struct {
+	PostgresqlHost     string
+	PostgresqlPort     string
+	PostgresqlUser     string
+	PostgresqlPassword string
+	PostgresqlDbname   string
+	PostgresqlSSLMode  bool
+	PgDriver           string
+}
+
+// Server config struct
+type ServerConfig struct {
+	AppVersion        string
+	Port              string
+	PprofPort         string
+	Mode              string
+	JwtSecretKey      string
+	CookieName        string
+	ReadTimeout       time.Duration
+	WriteTimeout      time.Duration
+	SSL               bool
+	CtxDefaultTimeout time.Duration
+	CSRF              bool
+	Debug             bool
+	MaxConnectionIdle time.Duration
+	Timeout           time.Duration
+	MaxConnectionAge  time.Duration
+	Time              time.Duration
+}
+
+// C is config variable
+var C config
+
+// ReadConfigOption is a config option
+type ReadConfigOption struct {
+	AppEnv string
+}
+
+// ReadConfig configures config file
+func ReadConfig(option ReadConfigOption) {
+	Config := &C
+
+	viper.AddConfigPath(filepath.Join(rootDir(), "config"))
+	viper.SetConfigName("config")
+
+	viper.SetConfigType("yml")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		log.Fatalln(err)
+	}
+
+	if err := viper.Unmarshal(&Config); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func rootDir() string {
+	_, b, _, _ := runtime.Caller(0)
+	d := path.Join(path.Dir(b))
+	return filepath.Dir(d)
+}
